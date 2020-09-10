ea52882f532fd5560e326af05ac0e8337e9b86a8
AbdelrahimNashif <abode.nashif11@gmail.com>
2020-09-10 09:38:16 +0300

    done antil the toast "done" again

--- app/src/main/java/suheil/aoc/login2/MainActivity.java
+++ app/src/main/java/suheil/aoc/login2/MainActivity.java
@@ -2,13 +2,33 @@ package suheil.aoc.login2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+private EditText first,last;
+private Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        first=findViewById(R.id.firstname);
+        last=findViewById(R.id.lastname);
+        btn=findViewById(R.id.loginbtn);
+        btn.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+if(view==btn){
+    Intent intent=new Intent(this,secActivityy.class);
+    intent.putExtra("firstName",first.getText().toString());
+    intent.putExtra("lastName",last.getText().toString());
+    startActivity(intent);
+}
     }
 }
---
+++ app/src/main/java/suheil/aoc/login2/secActivityy.java
@@ -0,0 +1,27 @@
+package suheil.aoc.login2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class secActivityy extends AppCompatActivity {
+private TextView tv;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sec_activityy);
+        tv=findViewById(R.id.tv);
+        Intent intent=getIntent();
+        String first=intent.getStringExtra("firstName");
+        String last=intent.getStringExtra("lastName");
+
+        if(first!=null&&last!=null) {
+            tv.setText(first + " " + last);
+            Toast.makeText(this,"done!!",Toast.LENGTH_LONG).show();
+
+        }
+    }
+}
